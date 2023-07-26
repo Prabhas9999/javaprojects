c1fdd642655c7ab9d297da3ffbfb4d3a418f54f2
Prabhas9999 <139949408+Prabhas9999@users.noreply.github.com>
2023-07-26 11:25:00 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        AddressBookView view = new AddressBookView();
-        AddressBookController controller = new AddressBookController(view);
-        controller.start();
-    }
-}
